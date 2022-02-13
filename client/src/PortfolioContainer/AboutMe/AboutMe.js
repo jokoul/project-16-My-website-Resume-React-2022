3c7292fd77d1f3668242dc4c9db7fd56cf9241ba
jokoul <74833985+jokoul@users.noreply.github.com>
2022-02-13 23:56:01 +0100

    end of the about me component

---
+++ client/src/PortfolioContainer/AboutMe/AboutMe.js
@@ -0,0 +1,70 @@
+import React from "react";
+import Animations from "../../utils/Animations";
+import ScreenHeading from "../../utils/ScreenHeading/ScreenHeading";
+import ScrollService from "../../utils/ScrollService";
+import "./AboutMe.css";
+
+export default function AboutMe(props) {
+  let fadeInScreenHandler = (screen) => {
+    if (screen.fadeScreen !== props.id) return;
+    Animations.animations.fadeInScreen(props.id);
+  };
+
+  const fadeInSubscription =
+    ScrollService.currentScreenFadeIn.subscribe(fadeInScreenHandler);
+
+  const SCREEN_CONSTANTS = {
+    description:
+      "As an experienced industrial qualitician, I chose in 2021 to retrain in the web development, a booming sector that has always fascinated me. Following my recent validation of a professional diploma of web developer, I am looking for a company to sign a 24-month professionalization contract with a rhythm of 3 days per week in the company. This will allow me to continue my work-study Java developer training.",
+    highlights: {
+      bullets: [
+        "Creating web application using framework like React.js and Vue.js",
+        "Building an API REST connected to a database for a web application using Node.js, Express.js, MySQL, MongoDB, etc.",
+        "Building an online shopping website using Javascript",
+        "Transforming a sketch model into a website with HTML, CSS, Bootstrap, Sass, etc.",
+        "Enhancing an existing website with SEO referencing tools ",
+      ],
+      heading: "Here are a few Highlights :",
+    },
+  };
+
+  const renderHighLight = () => {
+    return SCREEN_CONSTANTS.highlights.bullets.map((value, i) => (
+      <div className="highlight" key={i}>
+        <div className="highlight-blob"></div>
+        <span>{value}</span>
+      </div>
+    ));
+  };
+
+  return (
+    <div className="about-me-container screen-containre" id={props.id || ""}>
+      <div className="about-me-parent">
+        <ScreenHeading title={"About Me"} subHeading={"Why Choose Me"} />
+        <div className="about-me-card">
+          <div className="about-me-profile"></div>
+          <div className="about-me-details">
+            <div className="about-me-description">
+              <span>{SCREEN_CONSTANTS.description}</span>
+            </div>
+            <div className="about-me-highlights">
+              <div className="highlight-heading">
+                <span>{SCREEN_CONSTANTS.highlights.heading}</span>
+              </div>
+              {renderHighLight()}
+            </div>
+            <div className="about-me-options">
+              <button className="btn primary-btn">Hire Me </button>
+              <a
+                href="CV_Kouloumba_Joan.pdf"
+                download="Joan CV_Kouloumba_Joan.pdf"
+              >
+                <button className="btn highlighted-btn">Get Resume</button>
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
---
+++ client/src/PortfolioContainer/Resume/Resume.js
@@ -0,0 +1,42 @@
+import React, { useState } from "react";
+import Animations from "../../utils/Animations";
+import ScreenHeading from "../../utils/ScreenHeading/ScreenHeading";
+import ScrollService from "../../utils/ScrollService";
+import "./Resume.css";
+
+export default function Resume(props) {
+  const [selectedBulletIndex, setSelectedBulletIndex] = useState(0);
+  const [carousalOffSetStyle, setCarousalOffSetStyle] = useState({});
+  //console.log(props);
+  const ResumeHeading = (props) => {
+    <div className="resume-heading">
+      <div className="resume-main-heading">
+        <div className="heading-bullet">
+          <span>{props.heading ? props.heading : ""}</span>
+          {props.fromDate && props.toDate ? (
+            <div className="heading-date">
+              {props.fromDate + "_" + props.toDate}
+            </div>
+          ) : (
+            <div></div>
+          )}
+        </div>
+      </div>
+    </div>;
+  };
+  let fadeInScreenHandler = (screen) => {
+    if (screen.fadeScreen !== props.id) return;
+    Animations.animations.fadeInScreen(props.id);
+  };
+
+  const fadeInSubscription =
+    ScrollService.currentScreenFadeIn.subscribe(fadeInScreenHandler);
+
+  return (
+    <div className="resume-container screen-container" id={props.id || ""}>
+      <div className="resume-content">
+        <ScreenHeading title={"Resume"} subHeading={"My Formal Bio Details"} />
+      </div>
+    </div>
+  );
+}
