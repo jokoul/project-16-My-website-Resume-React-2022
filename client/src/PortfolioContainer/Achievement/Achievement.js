ccfb969c58cba4790e4657e9de936a8fb963e46b
jokoul <74833985+jokoul@users.noreply.github.com>
2022-03-18 11:09:45 +0100

    make it better

--- client/src/PortfolioContainer/Achievement/Achievement.js
+++ client/src/PortfolioContainer/Achievement/Achievement.js
@@ -19,9 +19,7 @@ export default function Achievement(props) {
         subHeading={"Take a look around all my projects achieved"}
       />
       <div className="btn-wrapper">
-        <a href="https://joan-kouloumba.in" target="_blank">
-          Explore my projects
-        </a>
+        <a href="https://joan-kouloumba.in">Explore my projects</a>
       </div>
     </div>
   );
